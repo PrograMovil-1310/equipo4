54055fe7027b8114fbec902656ea557fbe3b8552
Abimelec <abimelecg2@unitec.edu>
2023-12-03 16:49:18 -0600

    iconos

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -2,25 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class Home extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('QR Inventory'),
-        backgroundColor: Color.fromARGB(255, 167, 20, 20)
-      ),
-       drawer: MainDrawer(),
+          title: Text('QR Inventory'),
+          backgroundColor: Color.fromARGB(255, 167, 20, 20)),
+      drawer: MainDrawer(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
               'assets/logo.jpg',
-               height: 200.0,
-               width: 200.0,
-               fit: BoxFit.contain,
-              ),
+              height: 200.0,
+              width: 200.0,
+              fit: BoxFit.contain,
+            ),
             SizedBox(height: 16.0),
             Text(
               '¡Bienvenido a QR Inventory!',
@@ -41,7 +39,6 @@ class Home extends StatelessWidget {
           ],
         ),
       ),
- 
       bottomNavigationBar: BottomAppBar(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -52,7 +49,6 @@ class Home extends StatelessWidget {
                 Home();
               },
             ),
-            
           ],
         ),
       ),
@@ -60,28 +56,50 @@ class Home extends StatelessWidget {
   }
 }
 
-class MainDrawer extends StatelessWidget{
+class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-         UserAccountsDrawerHeader(
-              accountName: Text('MENU QR INVENTORY'),
-              accountEmail: Text('qrinventory@gmail.com'),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/logo.jpg'), 
-              ),
-            decoration: BoxDecoration( color: Color.fromARGB(255, 167, 20, 20),
+          UserAccountsDrawerHeader(
+            accountName: Text('MENU QR INVENTORY'),
+            accountEmail: Text('qrinventory@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage('assets/logo.jpg'),
             ),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 167, 20, 20),
             ),
-          DrawerItem(icon: Icons.home, text: 'Inicio', onTap: () => Navigator.pushNamed(context, '/home')),
-          DrawerItem(icon: Icons.feed, text: 'Scanner', onTap: () => Navigator.pushNamed(context, '/scanner')),
-          DrawerItem(icon: Icons.monetization_on, text: 'Inventario', onTap: () => Navigator.pushNamed(context, '/inventario')),
-          DrawerItem(icon: Icons.view_list_outlined, text: 'Editar', onTap: () => Navigator.pushNamed(context, '/editar')),
-          DrawerItem(icon: Icons.podcasts, text: 'Acerca de', onTap: () => Navigator.pushNamed(context, '/acerca de')),
-          ListTile(leading: Icon(Icons.exit_to_app), title: Text('Salir'), onTap: () { SystemNavigator.pop();},),
+          ),
+          DrawerItem(
+              icon: Icons.home,
+              text: 'Inicio',
+              onTap: () => Navigator.pushNamed(context, '/home')),
+          DrawerItem(
+              icon: Icons.qr_code_scanner,
+              text: 'Scanner',
+              onTap: () => Navigator.pushNamed(context, '/scanner')),
+          DrawerItem(
+              icon: Icons.inventory,
+              text: 'Inventario',
+              onTap: () => Navigator.pushNamed(context, '/inventario')),
+          DrawerItem(
+              icon: Icons.draw,
+              text: 'Editar',
+              onTap: () => Navigator.pushNamed(context, '/editar')),
+          DrawerItem(
+              icon: Icons.info,
+              text: 'Acerca de',
+              onTap: () => Navigator.pushNamed(context, '/acerca de')),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Salir'),
+            onTap: () {
+              SystemNavigator.pop();
+            },
+          ),
         ],
       ),
     );
@@ -89,9 +107,13 @@ class MainDrawer extends StatelessWidget{
 }
 
 class DrawerItem extends ListTile {
-  DrawerItem({required IconData icon, required String text, required VoidCallback onTap}) : super(
-    leading: Icon(icon),
-    title: Text(text),
-    onTap: onTap,
-  );
+  DrawerItem(
+      {required IconData icon,
+      required String text,
+      required VoidCallback onTap})
+      : super(
+          leading: Icon(icon),
+          title: Text(text),
+          onTap: onTap,
+        );
 }
